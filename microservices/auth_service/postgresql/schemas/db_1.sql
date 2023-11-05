6ee4a81c05c978f4fc38da462070f4c4d6c9dda8
root.kidik <root.kidik@gmail.com>
2023-11-05 19:05:31 +0700

    fix(`auth_service`): add `NOT NULL`

--- microservices/auth_service/postgresql/schemas/db_1.sql
+++ microservices/auth_service/postgresql/schemas/db_1.sql
@@ -3,6 +3,6 @@ DROP SCHEMA IF EXISTS auth_schema CASCADE;
 CREATE SCHEMA IF NOT EXISTS auth_schema;
 
 CREATE TABLE IF NOT EXISTS auth_schema.users (
-    email TEXT PRIMARY KEY,
+    email TEXT PRIMARY KEY NOT NULL,
     password TEXT NOT NULL 
 );
